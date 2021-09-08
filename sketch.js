0e72b8c954e4a2876458e101493d4d9d2f32fbe7
sbaksh4527 <78521742+sbaksh4527@users.noreply.github.com>
2021-09-08 10:11:50 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  fill('yellow');
+  strokeWeight(10);
+  rect(100,60,180,180);
+  fill(0);
+  line(100,140,30,80);
+  line(280,140,350,80);
+  line(140,240,140,340);
+  line(240,340,240,240);
+  strokeWeight(5);
+  fill(255);
+  ellipse(150,120,40,40);
+  ellipse(230,120,40,40);
+  fill(0);
+  ellipse(150,120,15,15);
+  ellipse(230,120,15,15);
+  fill(255);
+  rect(140,175,100,40);
+  line(180,140,210,160);
+  line(210,160,180,160);
+  
+}
